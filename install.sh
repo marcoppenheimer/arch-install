708691fb2b2ba6876236feaa53f04eb80983074c
marcoppenheimer <marcaoppenheimer@gmail.com>
2021-09-15 13:21:24 +0100

    Continuing build

--- install.sh
+++ install.sh
@@ -3,72 +3,54 @@
 # iwctl --passphrase PASSPHRASE station DEVICE connect SSID
 # https://github.com/classy-giraffe/easy-arch/blob/main/easy-arch.sh
 # https://github.com/picodotdev/alis/blob/master/alis.sh + https://github.com/picodotdev/alis/blob/master/alis.conf
+
 #Cleaning
 clear
+read -r -p "Please insert the keymap you use: " keymap 
+loadkeys $keymap
 
-# Selecting a kernel to install. 
-kernel_selector () {
-    echo "List of kernels:"
-    echo "1) Stable — Vanilla Linux kernel and modules, with a few patches applied."
-    echo "2) Hardened — A security-focused Linux kernel."
-    echo "3) Longterm — Long-term support (LTS) Linux kernel and modules."
-    echo "4) Zen Kernel — Optimized for desktop usage."
-    read -r -p "Insert the number of the corresponding kernel: " choice
-    echo "$choice will be installed"
+# Selecting display drivers. 
+display_drivers_selector () {
+    echo "List of display drivers:"
+    echo "1) intel" 
+    echo "2) nvidia" 
+    echo "3) nouveau" 
+    read -r -p "Insert the number of the corresponding display drivers: " choice
     case $choice in
-        1 ) kernel=linux
+        1 ) DISPLAY_DRVIER="intel"
+            PACKAGES_DRIVER_MULTILIB="lib32-mesa"
+            PACKAGES_HARDWARE_ACCELERATION="intel-media-driver"
+            PACKAGES_HARDWARE_ACCELERATION_MULTILIB=""
+            MKINITCPICO_KMS_MODULES="i915" 
+            echo "options i915 enable_fbc=1 fastboot=1" > /mnt/etc/modprobe.d/1915.conf
             ;;
-        2 ) kernel=linux-hardened
+        2 ) DISPLAY_DRVIER="nvidia"
+            PACKAGES_DRIVER="nvidia"
+            PACKAGES_DRIVER_MULTILIB="lib32-nvidia-utils"
+            PACKAGES_HARDWARE_ACCELERATION="libva-mesa-driver"
+            PACKAGES_HARDWARE_ACCELERATION_MULTILIB="lib32-libva-mesa-driver"
+            MKINITCPICO_KMS_MODULES="nvidia nvidia_modeset nvidia_uvm nvidia_drm" 
             ;;
-        3 ) kernel=linux-lts
-            ;;
-        4 ) kernel=linux-zen
+        3 ) DISPLAY_DRVIER="nouveau"
+            PACKAGES_DRIVER=""
+            PACKAGES_DRIVER_MULTILIB="lib32-mesa"
+            PACKAGES_HARDWARE_ACCELERATION="libva-mesa-driver"
+            PACKAGES_HARDWARE_ACCELERATION_MULTILIB="lib32-libva-mesa-driver"
+            MKINITCPICO_KMS_MODULES="nouveau" 
             ;;
         * ) echo "You did not enter a valid selection."
-            kernel_selector
+            display_drivers_selector
     esac
 }
 
-# Selecting a way to handle internet connection. 
-network_selector () {
-    echo "Network utilities:"
-    echo "1) IWD — iNet wireless daemon is a wireless daemon for Linux written by Intel (WiFi-only)."
-    echo "2) NetworkManager — Program for providing detection and configuration for systems to automatically connect to networks (both WiFi and Ethernet)."
-    echo "3) wpa_supplicant — It's a cross-platform supplicant with support for WEP, WPA and WPA2 (WiFi-only, a DHCP client will be automatically installed too.)"
-    echo "4) I will do this on my own."
-    read -r -p "Insert the number of the corresponding networking utility: " choice
-    echo "$choice will be installed"
-    case $choice in
-        1 ) echo "Installing IWD."    
-            pacstrap /mnt iwd
-            echo "Enabling IWD."
-            systemctl enable iwd --root=/mnt &>/dev/null
-            ;;
-        2 ) echo "Installing NetworkManager."
-            pacstrap /mnt networkmanager
-            echo "Enabling NetworkManager."
-            systemctl enable NetworkManager --root=/mnt &>/dev/null
-            ;;
-        3 ) echo "Installing wpa_supplicant and dhcpcd."
-            pacstrap /mnt wpa_supplicant dhcpcd
-            echo "Enabling wpa_supplicant and dhcpcd."
-            systemctl enable wpa_supplicant --root=/mnt &>/dev/null
-            systemctl enable dhcpcd --root=/mnt &>/dev/null
-            ;;
-        4 )
-            ;;
-        * ) echo "You did not enter a valid selection."
-            network_selector
-    esac
+# ------------------ STARTING ----------------------------------------------------------------
+exec > >(tee -a build.log)
+exec 2> >(tee -a build.log >&2)
+set -o xtrace
 
-# Checking the microcode to install.
-CPU=$(grep vendor_id /proc/cpuinfo)
-if [[ $CPU == *"AuthenticAMD"* ]]
-then
-    microcode=amd-ucode
-else
-    microcode=intel-ucode
-fi
+#Configure time
+timedatectl set-timezone Europe/London
+timedatectl set-ntp true
 
 # Selecting the target for the installation.
 PS3="Select the disk where Arch Linux is going to be installed: "
@@ -90,3 +72,187 @@ else
     echo "Quitting."
     exit
 fi
+
+# Creating a new partition scheme.
+echo "Creating new partition scheme on $DISK."
+parted -s "$DISK" \
+    mkpart "efi" fat32 1MiB 513MiB \
+    set 1 esp on \
+    mkpart "swap" linux-swap 513MiB 7513MiB \
+    mkpart "root" ext4 7513MiB 100%
+
+efi="/dev/disk/by-partlabel/efi"
+swap="/dev/disk/by-partlabel/swap"
+root="/dev/disk/by-partlabel/root"
+
+# Informing the Kernel of the changes.
+echo "Informing the Kernel about the disk changes."
+partprobe "$DISK"
+
+# Formatting the EFI as FAT32.
+echo "Formatting the EFI Partition as FAT32."
+mkfs.fat -F32 $efi &>/dev/null
+
+# Formatting the swap as FAT32.
+echo "Formatting the swap Partition."
+mkswap $swap &>/dev/null
+
+# Formatting the ESP as FAT32.
+echo "Formatting the root Partition as ext4."
+mkfs.ext4 $root &>/dev/null
+
+# Mounting.
+mkdir /mnt/efi
+mount $efi /mnt/efi
+swapon $swap
+mount $root /mnt &>/dev/null
+
+
+
+# Installation.
+echo "Server = 'https://mirrors.kernel.org/archlinux/\$repo/os/\$arch" > /etc/pacman.d/mirrorlist
+kernel_selector
+sed -i 's/#Color/Color/' etc/pacman.conf
+sed -i 's/#ParallelDownloads/ParallelDownloads/' /etc/pacman.conf
+pacstrap /mnt base linux linux-firmware base-devel
+echo "" >> /mnt/etc/pacman.conf
+echo "[multilib]" >> /mnt/etc/pacman.conf
+echo "Include = /etc/pacman.d/mirrorlist" >> /mnt/etc/pacman.conf
+echo "" >> /mnt/etc/pacman.conf
+
+# Setting Timezone and localisation.
+read -r -p "Please insert the region and city you use (format: Region/City): " region_city
+arch-chroot /mnt ln -s -f /usr/share/zoneinfo/$region_city /etc/localtime
+arch-chroot /mnt hwclock --systoch 
+read -r -p "Please insert the locale you use (format: xx_XX): " locale
+echo "$locale.UTF-8 UTF-8"  > /etc/locale.gen
+echo "$locale.UTF-8 UTF-8"  > /mnt/etc/locale.gen
+echo "LANG=$locale.UTF-8" > /mnt/etc/locale.conf
+locale-gen
+arch-chroot /mnt locale-gen
+echo "KEYMAP=$keymap" > /mnt/etc/vconsole.conf
+
+
+# Honestly not sure what this is.
+arch-chroot /mnt mkdir -p "/etc/X11/xorg.conf.d/"
+    cat <<EOT > /mnt/etc/X11/xorg.conf.d/00-keyboard.conf
+# Written by systemd-localed(8), read by systemd-localed and Xorg. It's
+# probably wise not to edit this file manually. Use localectl(1) to
+# instruct systemd-localed to update it.
+Section "InputClass"
+    Identifier "system-keyboard"
+    MatchIsKeyboard "on"
+    $OPTIONS
+EndSection
+EOT
+
+# Setting swappiness.
+echo "vm.swappiness-10" > /mnt/etc/sysctl.d/99-sysctl.conf
+
+# Setting hostname and password.
+read -r -p "Please enter your desired hostname: " hostname
+echo "$hostname" > /mnt/etc/hostname
+read -r -p "Please enter your desired root password: " password 
+printf "$password\n$password" | arch-chroot /mnt passwd
+
+# Generating /etc/fstab.
+echo "Generating a new fstab."
+genfstab -U /mnt >> /mnt/etc/fstab
+echo "# swap" >> /mnt/etc/fstab
+echo "/swapfile none swap defaults 0 0" >> /mnt/etc/fstab
+echo "" >> /mnt/etc/fstab
+
+# Setting hosts file.
+echo "Setting hosts file."
+cat > /mnt/etc/hosts <<EOF
+127.0.0.1   localhost
+::1         localhost
+127.0.1.1   $hostname.localdomain   $hostname
+EOF
+
+# Installing display drivers.
+echo "Installing display drivers."
+display_drivers_selector
+pacman_install "mesa $PACKAGES_DRIVER $PACKAGES_HARDWARE_ACCELERATION $PACKAGES_DRIVER_MULTILIB $PACKAGES_HARDWARE_ACCELERATION_MULTILIB"  
+
+# Configuring /etc/mkinitcpio.conf
+echo "Configuring /etc/mkinitcpio.conf"
+HOOKS="base udev usr keyboard autodetect modconf block keymap consolefont fsck filesystems"
+arch-chroot /mnt sed -i "s/^HOOKS=(.*)/HOOKS=($HOOKS)/" /etc/mkinitcpio.conf
+arch-chroot /mnt sed -i "s/^MODULES=(.*)/MODULES=($MKINITCPICO_KMS_MODULES)/" /etc/mkinitcpio.conf
+
+# Installing kernel.
+echo "Installing kernel."
+pacman_install "linux-headers"
+pacman_install "linux-zen"  
+
+# Setting mkinitcpio.
+echo "Setting mkinitcpio."
+arch-chroot /mnt mkinitcpio -P
+
+# Installing network.
+echo "Installing network."
+pacman_install "networkmanager"
+arch-chroot /mnt systemctl enable NetworkManager.service
+
+# Creating personal user and groups.
+echo "Creating personal user and groups."
+read -r -p "Please enter your desired username: " username
+read -r -p "Please enter your desired password" user_password
+arch-chroot /mnt useradd -m -G "wheel,storage,optical" -s /bin/bash $username
+printf "$user_password\n$user_password" | arch-chroot /mnt passwd $username
+arch-chroot /mnt sed -i 's/# %wheel ALL=(ALL) ALL/%wheel ALL=(ALL) ALL/' /etc/sudoers
+pacman_install "xdg-user-dirs"
+
+# TODO: - YOU GOT HERE YOU SHIT
+# Bootloader
+CPU=$(grep vendor_id /proc/cpuinfo)
+if [[ $CPU == *"AuthenticAMD"* ]]
+then
+    pacman_install "amd-ucode"
+else
+    pacman_install "intel-ucode"
+fi
+case "$DISPLAY_DRVIER" in "nvidia" | "nvidia-390xx" | "nvidia-390xx-lts" )
+    CMDLINE_LINUX="$CMDLINE_LINUX nvidia-drm.modeset=1"
+    ;;
+esac
+if [[ $DISK == *"nvme"* ]]
+then
+    KERNELS_PARAMETERS="nvme_load=YES"
+pacman_install "refind"
+arch-chroot /mnt refind-install
+arch-chroot /mnt sed -i 's/^timeout.*/timeout 5/' "$efi/EFI/refind/refind.conf"
+arch-chroot /mnt sed -i 's/^#scan_all_linux_kernels.*/scan_all_linux_kernels false/' "$efi/EFI/refind/refind.conf"
+cat <<EOT >> "/mnt$efi/EFI/refind/refind.conf"
+# alis
+menuentry "Arch Linux" {
+    volume   $PARTUUID_BOOT
+    loader   /vmlinuz-linux
+    initrd   /initramfs-linux.img
+    icon     /EFI/refind/icons/os_arch.png
+    options  "$REFIND_MICROCODE $CMDLINE_LINUX_ROOT rw $CMDLINE_LINUX"
+    submenuentry "Boot using fallback initramfs"
+	      initrd /initramfs-linux-fallback.img"
+    }
+    submenuentry "Boot to terminal"
+	      add_options "systemd.unit=multi-user.target"
+    }
+}
+EOT
+
+arch-chroot /mnt systemctl set-default multi-user.target
+
+# Installing custom shell.
+echo "Installing custom shell."
+pacman_install "zsh"
+CUSTOM_SHELL_PATH="/usr/bin/zsh"
+arch-chroot /mnt chsh -s "/usr/bin/zsh" "root" 
+arch-chroot /mnt chsh -s "/usr/bin/zsh" $username 
+
+# Installing WM/DE.
+echo "Installing WM/DE."
+pacman_install "i3-gaps i3blocks i3lock i3status dmenu rxvt-unicode lightdm lightdm-gtk-greeter xorgserver"
+arch-chroot /mnt systemctl enable lightdm.service
+
+# efibootmgr $microcode $kernel neovim man-db man-pages texinfo 
